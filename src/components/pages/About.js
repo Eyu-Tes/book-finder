a29b223e891b25c623d9ce9bd8e6f4f6e1688aea
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2020-10-26 18:23:06 +0300

    added about page

---
+++ src/components/pages/About.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+
+const About = () => 
+    <Fragment>
+        <h2 className="mb-3">About Book Finder</h2>
+        <p>Book Finder is a React.js app that uses the Google Books API to search books.</p>
+        <p>Version: 1.0.0</p>
+    </Fragment>
+
+export default About

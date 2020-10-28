9d19a20368d107c5eb66d3323b744a339530d198
Eyoab Tesfaye <eyoabtesfaye10@gmail.com>
2020-10-28 19:05:23 +0300

    change Search component from class based to functional

--- src/components/books/Search.js
+++ src/components/books/Search.js
@@ -1,59 +1,53 @@
-import React, {Component, Fragment} from 'react'
+import React, {Fragment, useState} from 'react'
 import PropTypes from 'prop-types'
 import './Search.css'
 
-class Search extends Component {
-    static propTypes = {
-        searchBooks: PropTypes.func.isRequired,
-        clearBooks: PropTypes.func.isRequired,
-        showClear: PropTypes.bool.isRequired,
-        showAlert: PropTypes.func.isRequired,
-    }
+const Search = ({showClear, clearBooks, showAlert, searchBooks}) => {
+    const [text, setText] = useState('')
 
-    state = {
-        text: '',
-    }
+    const onChange = (e) => setText(e.target.value)
 
-    /* Make onChange work for any number of input elements using computed property names [] */
-    onChange = (e) => this.setState({[e.target.name]: e.target.value})
-
-    onSubmit = (e) => {
+    const onSubmit = (e) => {
         e.preventDefault()
-        if (this.state.text.trim() === '') {
-            this.props.showAlert('Please enter something', 'light')
+        if (text.trim() === '') {
+            showAlert('Please enter something', 'light')
         }
         else {
-            this.props.searchBooks(this.state.text)
-            this.setState({text: ''})
+            searchBooks(text)
+            setText('')
         }
     }
 
-    render() {
-        const {showClear, clearBooks} = this.props
-        return(
-            <Fragment>
-                <form id="book-form" className="mt-3" onSubmit={this.onSubmit}>
-                    <div className="form-group">
-                        <input 
-                            name="text" 
-                            type="text" 
-                            className="form-control" 
-                            autoFocus
-                            placeholder='Search books ...'
-                            value={this.state.text}
-                            onChange={this.onChange}
-                        />
-                    </div>
-                    <div className="form-group">
-                        <button type="submit" className="btn btn-block btn-dark rounded-0">Search</button>
-                        {showClear && (
-                            <button type="button" onClick={clearBooks} className="btn btn-block btn-light rounded-0">Clear</button>
-                        )}
-                    </div>
-                </form>
-            </Fragment>
-        );
-    }
+    return(
+        <Fragment>
+            <form id="book-form" className="mt-3" onSubmit={onSubmit}>
+                <div className="form-group">
+                    <input 
+                        name="text" 
+                        type="text" 
+                        className="form-control" 
+                        autoFocus
+                        placeholder='Search books ...'
+                        value={text}
+                        onChange={onChange}
+                    />
+                </div>
+                <div className="form-group">
+                    <button type="submit" className="btn btn-block btn-dark rounded-0">Search</button>
+                    {showClear && (
+                        <button type="button" onClick={clearBooks} className="btn btn-block btn-light rounded-0">Clear</button>
+                    )}
+                </div>
+            </form>
+        </Fragment>
+    );
+}
+
+Search.propTypes = {
+    searchBooks: PropTypes.func.isRequired,
+    clearBooks: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    showAlert: PropTypes.func.isRequired,
 }
 
 export default Search
